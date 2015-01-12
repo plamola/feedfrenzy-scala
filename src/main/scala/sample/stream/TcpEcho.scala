75d7c470c4e17449bd443508136a3d14019ae86c
matthijs <matthijs@plamola.com>
2015-01-12 15:56:38 +0100

    Disabled

--- src/main/scala/sample/stream/TcpEcho.scala
+++ src/main/scala/sample/stream/TcpEcho.scala
@@ -21,67 +21,67 @@ object TcpEcho {
    * server on 127.0.0.1:6001.
    *
    */
-  def main(args: Array[String]): Unit = {
-    if (args.isEmpty) {
-      val system = ActorSystem("ClientAndServer")
-      val serverAddress = new InetSocketAddress("127.0.0.1", 6000)
-      server(system, serverAddress)
-      client(system, serverAddress)
-    } else {
-      val serverAddress =
-        if (args.length == 3) new InetSocketAddress(args(1), args(2).toInt)
-        else new InetSocketAddress("127.0.0.1", 6000)
-      if (args(0) == "server") {
-        val system = ActorSystem("Server")
-        server(system, serverAddress)
-      } else if (args(0) == "client") {
-        val system = ActorSystem("Client")
-        client(system, serverAddress)
-      }
-    }
-  }
-
-  def server(system: ActorSystem, serverAddress: InetSocketAddress): Unit = {
-    implicit val sys = system
-    import system.dispatcher
-    implicit val materializer = FlowMaterializer()
-
-    val handler = ForeachSink[StreamTcp.IncomingConnection] { conn =>
-      println("Client connected from: " + conn.remoteAddress)
-      conn handleWith Flow[ByteString]
-    }
-
-    val binding = StreamTcp().bind(serverAddress)
-    val materializedServer = binding.connections.to(handler).run()
-
-    binding.localAddress(materializedServer).onComplete {
-      case Success(address) =>
-        println("Server started, listening on: " + address)
-      case Failure(e) =>
-        println(s"Server could not bind to $serverAddress: ${e.getMessage}")
-        system.shutdown()
-    }
-
-  }
-
-  def client(system: ActorSystem, serverAddress: InetSocketAddress): Unit = {
-    implicit val sys = system
-    import system.dispatcher
-    implicit val materializer = FlowMaterializer()
-
-    val testInput = ('a' to 'z').map(ByteString(_))
-
-    val result = Source(testInput).via(StreamTcp().outgoingConnection(serverAddress).flow).
-      fold(ByteString.empty) { (acc, in) ⇒ acc ++ in }
-
-    result.onComplete {
-      case Success(result) =>
-        println(s"Result: " + result.utf8String)
-        println("Shutting down client")
-        system.shutdown()
-      case Failure(e) =>
-        println("Failure: " + e.getMessage)
-        system.shutdown()
-    }
-  }
+  //  def main(args: Array[String]): Unit = {
+  //    if (args.isEmpty) {
+  //      val system = ActorSystem("ClientAndServer")
+  //      val serverAddress = new InetSocketAddress("127.0.0.1", 6000)
+  //      server(system, serverAddress)
+  //      client(system, serverAddress)
+  //    } else {
+  //      val serverAddress =
+  //        if (args.length == 3) new InetSocketAddress(args(1), args(2).toInt)
+  //        else new InetSocketAddress("127.0.0.1", 6000)
+  //      if (args(0) == "server") {
+  //        val system = ActorSystem("Server")
+  //        server(system, serverAddress)
+  //      } else if (args(0) == "client") {
+  //        val system = ActorSystem("Client")
+  //        client(system, serverAddress)
+  //      }
+  //    }
+  //  }
+  //
+  //  def server(system: ActorSystem, serverAddress: InetSocketAddress): Unit = {
+  //    implicit val sys = system
+  //    import system.dispatcher
+  //    implicit val materializer = FlowMaterializer()
+  //
+  //    val handler = ForeachSink[StreamTcp.IncomingConnection] { conn =>
+  //      println("Client connected from: " + conn.remoteAddress)
+  //      conn handleWith Flow[ByteString]
+  //    }
+  //
+  //    val binding = StreamTcp().bind(serverAddress)
+  //    val materializedServer = binding.connections.to(handler).run()
+  //
+  //    binding.localAddress(materializedServer).onComplete {
+  //      case Success(address) =>
+  //        println("Server started, listening on: " + address)
+  //      case Failure(e) =>
+  //        println(s"Server could not bind to $serverAddress: ${e.getMessage}")
+  //        system.shutdown()
+  //    }
+  //
+  //  }
+  //
+  //  def client(system: ActorSystem, serverAddress: InetSocketAddress): Unit = {
+  //    implicit val sys = system
+  //    import system.dispatcher
+  //    implicit val materializer = FlowMaterializer()
+  //
+  //    val testInput = ('a' to 'z').map(ByteString(_))
+  //
+  //    val result = Source(testInput).via(StreamTcp().outgoingConnection(serverAddress).flow).
+  //      fold(ByteString.empty) { (acc, in) ⇒ acc ++ in }
+  //
+  //    result.onComplete {
+  //      case Success(result) =>
+  //        println(s"Result: " + result.utf8String)
+  //        println("Shutting down client")
+  //        system.shutdown()
+  //      case Failure(e) =>
+  //        println("Failure: " + e.getMessage)
+  //        system.shutdown()
+  //    }
+  //  }
 }
