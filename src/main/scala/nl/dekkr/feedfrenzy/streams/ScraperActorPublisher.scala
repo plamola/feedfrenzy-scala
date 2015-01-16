d6ddfcc42c9520f6e806e733ff963a279384eee5
Matthijs <matthijs@plamola.com>
2015-01-16 09:58:01 +0100

    Some refactoring

--- src/main/scala/nl/dekkr/feedfrenzy/streams/ScraperActorPublisher.scala
+++ src/main/scala/nl/dekkr/feedfrenzy/streams/ScraperActorPublisher.scala
@@ -2,11 +2,7 @@ package nl.dekkr.feedfrenzy.streams
 
 import akka.event.Logging
 import akka.stream.actor.{ ActorPublisher, ActorPublisherMessage }
-import nl.dekkr.feedfrenzy.db.{ Schema, Tables }
-import nl.dekkr.feedfrenzy.model.{ Feed, Scraper }
-
-import scala.slick.driver.PostgresDriver.simple._
-import scala.slick.lifted.TableQuery
+import nl.dekkr.feedfrenzy.model.{ Syndication, Scraper }
 import scala.util.{ Failure, Success, Try }
 
 /**
@@ -19,14 +15,17 @@ class ScraperActorPublisher extends ActorPublisher[Scraper] {
   import scala.concurrent.duration._
 
   implicit val ec = context.dispatcher
-
-  val getNewJobs = context.system.scheduler.schedule(1 second, 1 minute, self, NewJobListing)
-  val log = Logging(context.system, this)
-
-  var scrapers = List.empty[Scraper]
+  val getNewJobs = context.system.scheduler.schedule(1 second, 1 second, self, NewJobListing)
   var reloadScrapers = true
 
+  val log = Logging(context.system, this)
+  var scrapers = List.empty[Scraper]
+
   def receive = {
+    case NewJobListing() =>
+      log.info("Received NewJobListing message")
+      reloadScrapers = true
+
     case ActorPublisherMessage.Request(n) =>
       while (isActive && totalDemand > 0) {
         generateElement() match {
@@ -42,14 +41,10 @@ class ScraperActorPublisher extends ActorPublisher[Scraper] {
       cleanupResources()
     case ActorPublisherMessage.SubscriptionTimeoutExceeded =>
       cleanupResources()
-    case NewJobListing() => {
-      log.info("Received NewJobListing message")
-      reloadScrapers = true
-    }
 
   }
 
-  override def preStart(): Unit = {
+  override def preStart() = {
     createResources()
   }
 
@@ -80,43 +75,9 @@ class ScraperActorPublisher extends ActorPublisher[Scraper] {
 
   def createResources() = {
     if (scrapers.size == 0 && reloadScrapers) {
-
       log.info("Getting new resources")
       reloadScrapers = false
-      scrapers = getUpdatableFeeds.map(feed => Scraper(id = feed.id, sourceUrl = feed.feedurl, singlePage = true))
-    }
-  }
-
-  def getUpdatableFeeds: List[Feed] = {
-    try {
-      val feeds = TableQuery[Tables.FeedTable]
-      implicit val session = Schema.getSession
-      Schema.createOrUpdate(session)
-
-      // Add some dummy feeds
-      if (feeds.list.size < 1)
-        feeds += Feed(feedurl = "http://nu.nl")
-
-      if (feeds.list.size < 2)
-        feeds += Feed(feedurl = "http://dagartikel.nl")
-
-      if (feeds.list.size < 3)
-        feeds += Feed(feedurl = "http://news.google.com")
-
-      if (feeds.list.size < 4)
-        feeds += Feed(feedurl = "http://news.yahoo.com")
-
-      if (feeds.list.size < 5)
-        feeds += Feed(feedurl = "http://www.theverge.com/")
-
-      if (feeds.list.size < 6)
-        feeds += Feed(feedurl = "http://news.ycombinator.com/")
-
-      feeds.list
-    } catch {
-      case e: Exception =>
-        println(s"ERROR: ${e.getMessage} [${e.getCause}]")
-        List.empty
+      scrapers = Syndication.getRunnableScrapers
     }
   }
 
