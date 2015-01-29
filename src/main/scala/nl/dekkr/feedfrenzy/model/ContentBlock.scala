aff11f3532b9a8d7b63797f19c7e958949e4c6d5
Matthijs <matthijs@plamola.com>
2015-01-29 14:35:36 +0100

    Added FK relation between Feed and Scraper

--- src/main/scala/nl/dekkr/feedfrenzy/model/ContentBlock.scala
+++ src/main/scala/nl/dekkr/feedfrenzy/model/ContentBlock.scala
@@ -3,4 +3,9 @@ package nl.dekkr.feedfrenzy.model
 /**
  * Created by Matthijs Dekker on 26/01/15.
  */
-case class ContentBlock(scraper: Scraper, content: Option[String] = None, uid: Option[String] = None, uri: Option[String] = None)
+case class ContentBlock(
+  scraper: Scraper,
+  actions: List[ScraperAction] = List.empty,
+  content: Option[String] = None,
+  uid: Option[String] = None,
+  uri: Option[String] = None)
