64c738f4cb9155b5b5c12bc82edcfa569439150e
Matthijs <matthijs@plamola.com>
2015-03-02 08:15:41 +0100

    Updated file headers

--- src/main/scala/nl/dekkr/feedfrenzy/model/Article.scala
+++ src/main/scala/nl/dekkr/feedfrenzy/model/Article.scala
@@ -3,6 +3,6 @@ package nl.dekkr.feedfrenzy.model
 import org.joda.time.DateTime
 
 /**
- * Created by Matthijs Dekker on 26/01/15.
+ * Article definition, based on RSS article def
  */
 case class Article(id: Option[Int] = None, feedid: Option[Int] = None, uri: String, link: Option[String] = None, title: Option[String] = None, content: Option[String] = None, author: Option[String] = None, publisheddate: Option[DateTime] = None, updateddate: Option[DateTime] = None, lastsynceddate: Option[DateTime] = None)
